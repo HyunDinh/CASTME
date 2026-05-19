8fafec52924950927926720d3f4628cec16dfd50
hyundinh <dinhhung1112005@gmail.com>
2026-05-19 22:28:12 +0700

    fixing effort #1

--- src/app/(shop)/messages/page.js
+++
@@ -1,8 +0,0 @@
-export default function Messages() {
-  return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold text-green-700">Messages</h1>
-      <p>Trang chat của shop</p>
-    </div>
-  );
-}
